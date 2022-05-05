d8c7471e4ce52ba5bffc8866b098ec9813fb0297
jawherkl <jawa.kalel@gmail.com>
2022-05-05 15:38:56 +0100

    Second Commit Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from '././service/token-storage.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'worldcupFront-end';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  adminPermission : boolean = false ;
+
+  user : any ;
+  constructor(private tokenStorageService: TokenStorageService) { }
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      this.user = this.tokenStorageService.getUser();
+      this.roles = this.user.roles;
+      this.adminPermission = this.permissions();
+     // this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+     // this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+    }
+
+  }
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
+
+  public permissions(): boolean
+  {
+    return this.user.roles.includes("ROLE_ADMIN");
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from '././app-routing.module';
+import {HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import {authInterceptorProviders } from './_helpers/auth.interceptor';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule ,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
